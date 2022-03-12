cd6815ce277c57c3317337fdd18c082b03bbf27c
LizardLiang <shotup0101@gmail.com>
2022-03-12 13:38:38 +0800

    add more details

--- pages/details.js
+++ pages/details.js
@@ -2,7 +2,7 @@ import { useContext } from 'react'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { Title, Thumbnail, InfoBox } from '../components/detail'
-import { Container, Box, Badge, Text } from '@chakra-ui/react'
+import { Container } from '@chakra-ui/react'
 import { DogInfo } from '../lib/dog-context'
 
 const LookupTable = {
@@ -53,19 +53,56 @@ const LookupTable = {
     53: '新北市中和區公立動物之家',
     55: '新北市淡水區公立動物之家',
     56: '新北市瑞芳區公立動物之家',
+    58: '新北市五股區公立動物之家',
+    59: '新北市八里區公立動物之家',
+    60: '新北市三芝區公立動物之家',
+    61: '桃園市動物保護教育園區',
+    62: '新竹市動物收容所',
+    63: '新竹縣動物收容所',
+    67: '台中市動物之家南屯園區',
+    68: '台中市動物之家后里園區',
+    69: '彰化縣流浪狗中途之家',
+    70: '南投縣公立動物收容所',
     71: '嘉義市流浪犬收容中心',
     72: '嘉義市流浪犬中途之家',
     73: '台南市動物之家灣理站',
     74: '台南市動物之家善化站',
     75: '高雄市壽山動物保護教育園區',
-    76: '高雄市燕巢動物保護�愛園區',
-    77: '屏東縣流浪動物收容所'
+    76: '高雄市燕巢動物保護關愛園區',
+    77: '屏東縣流浪動物收容所',
+    78: '宜蘭縣流浪動物中途之家',
+    79: '花蓮縣流浪犬中途之家',
+    80: '台東縣動物收容中心',
+    81: '連江縣流浪犬收容中心',
+    82: '金門縣動物收容中心',
+    83: '澎湖縣流浪動物收容中心',
+    89: '雲林縣流浪動物收容中心',
+    92: '新北市政府動物保護防疫處',
+    96: '苗栗縣生態保育教育中心'
+  },
+  age: {
+    UNDEFINE: '未輸入',
+    CHILD: '幼年',
+    ADULT: '成年'
+  },
+  status: {
+    NONE: '未公告',
+    OPEN: '開放認養',
+    ADOPTED: '已認養',
+    OTHER: '其他',
+    DEAD: '死亡'
+  },
+  bacterin: {
+    T: '是',
+    F: '否',
+    N: '未輸入'
   }
 }
 
 const Details = ({}) => {
   const { info } = useContext(DogInfo)
   const placeholder = '/images/dogPlaceholder.png'
+
   return (
     <Layout>
       <Container>
@@ -79,18 +116,28 @@ const Details = ({}) => {
         </Section>
         <Section delay={0.6}>
           <InfoBox
+            title={`STATUS`}
+            text={LookupTable.status[info.animal_status]}
+          />
+          <InfoBox
             title={`City`}
             text={LookupTable.areaPkid[info.animal_area_pkid]}
           />
           <InfoBox
             title={`Shelter`}
-            text={LookupTable.areaPkid[info.animal_area_pkid]}
+            text={LookupTable.shelter[info.animal_shelter_pkid]}
           />
+          <InfoBox title={`Shelter Name`} text={info.shelter_name} />
+          <InfoBox title={`Shelter Address`} text={info.shelter_address} />
+          <InfoBox title={`Shelter Phone`} text={info.shelter_tel} />
+          <InfoBox title={`Place`} text={info.animal_place} />
           <InfoBox title={`Open Date`} text={info.animal_opendate} />
           <InfoBox
             title={`Animal Size`}
             text={LookupTable.bodyType[info.animal_bodytype]}
           />
+          <InfoBox title={`Colour`} text={info.animal_colour} />
+          <InfoBox title={`Age`} text={LookupTable.age[info.animal_age]} />
           <InfoBox
             title={`Animal Sex`}
             text={LookupTable.sex[info.animal_sex]}
@@ -99,6 +146,14 @@ const Details = ({}) => {
             title={`Animal Sterilization`}
             text={LookupTable.sterilized[info.animal_sterilization]}
           />
+          <InfoBox title={`Open Date`} text={info.animal_opendate} />
+          <InfoBox title={`Close Date`} text={info.animal_closedate} />
+          <InfoBox
+            title={`Bacterin`}
+            text={LookupTable.bacterin[info.animal_bacterin]}
+          />
+          <InfoBox title={`Remark`} text={info.animal_remark} />
+          <InfoBox title={`Caption`} text={info.animal_caption} />
         </Section>
       </Container>
     </Layout>
@@ -106,4 +161,3 @@ const Details = ({}) => {
 }
 
 export default Details
-
